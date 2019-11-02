15151c8ea147e5547039508d7a60ba8dd77313ce
Christian Roque <croqu008@fiu.edu>
2019-11-02 22:21:45 +0000

    bare bones

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,4 @@
-const express = require('express')
-const { isLoggedIn } = require('../middlewares')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-router.get('/secret', isLoggedIn, (req, res, next) => {
-  res.json({
-    secret: 42,
-    user: req.user,
-  })
-})
-
-module.exports = router
+module.exports = router;
